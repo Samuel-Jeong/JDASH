1edae5ec348dbdd8b0cd401acd4b223591abd5ba
ehddnr177 <ehddnr177@naver.com>
2022-02-09 14:31:19 +0900

    # 변경사항 1. gc log 옵션 수정

--- framework/src/main/resources/bin/run.sh
+++ framework/src/main/resources/bin/run.sh
@@ -5,7 +5,7 @@ SERVICE_NAME=jdash
 PATH_TO_JAR=$SERVICE_HOME/lib/jdash_server.jar
 JAVA_CONF=$SERVICE_HOME/config/user_conf.ini
 JAVA_OPT="-Dlogback.configurationFile=$SERVICE_HOME/config/logback.xml"
-JAVA_OPT="$JAVA_OPT -XX:+UseG1GC -XX:G1RSetUpdatingPauseTimePercent=5 -XX:MaxGCPauseMillis=500 -Xlog:gc* -verbosegc"
+JAVA_OPT="$JAVA_OPT -XX:+UseG1GC -XX:G1RSetUpdatingPauseTimePercent=5 -XX:MaxGCPauseMillis=500 -verbosegc -Xlog:gc*:file=$SERVICE_HOME/logs/gc_%t.log:time:filecount=10,filesize=5M"
 
 function exec_start() {
 	ulimit -n 65535
