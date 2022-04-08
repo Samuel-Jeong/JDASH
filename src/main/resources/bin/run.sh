077bcbac7a04f71976f0b177478e7c8b90a5ab09
ehddnr177 <ehddnr177@naver.com>
2022-04-08 14:49:48 +0900

    # 변경사항 1. run 스크립트 수정

--- src/main/resources/bin/run.sh
+++ src/main/resources/bin/run.sh
@@ -2,31 +2,26 @@
 
 SERVICE_HOME=/home/uangel/jdash
 SERVICE_NAME=jdash
-#LD_LIBRARY_PATH=$SERVICE_HOME/lib
-#export LD_LIBRARY_PATH
-#PATH_TO_JAR=$SERVICE_HOME/lib/JDASH-1.0.0.jar
 PATH_TO_JAR=$SERVICE_HOME/lib/JDASH.jar
 JAVA_CONF=$SERVICE_HOME/config/user_conf.ini
 JAVA_OPT="-Dlogback.configurationFile=$SERVICE_HOME/config/logback.xml"
-#JAVA_OPT="$JAVA_OPT -Djava.library.path=$SERVICE_HOME/lib"
 JAVA_OPT="$JAVA_OPT -XX:+UseG1GC -XX:G1RSetUpdatingPauseTimePercent=5 -XX:MaxGCPauseMillis=500 -XX:+UseLargePages -verbosegc -Xms4G -Xmx4G -verbose:gc -Xms4G -Xmx4G -Xlog:gc=debug:file=$SERVICE_HOME/logs/gc.log:time,uptime,level,tags:filecount=5,filesize=100m"
 
 function exec_start() {
         PID=`ps -ef | grep java | grep DashServerMain | awk '{print $2}'`
         if ! [ -z "$PID" ]
         then
-                echo "DASH is already running"
+                echo "[$SERVICE_NAME] is already running"
         else
-                ulimit -n 65535
-                ulimit -s 65535
-                ulimit -u 10240
-                ulimit -Hn 65535
-                ulimit -Hs 65535
-                ulimit -Hu 10240
+                #ulimit -n 65535
+                #ulimit -s 65535
+                #ulimit -u 10240
+                #ulimit -Hn 65535
+                #ulimit -Hs 65535
+                #ulimit -Hu 10240
 
                 /usr/lib/jvm/java-11/bin/java -jar $JAVA_OPT $PATH_TO_JAR DashServerMain $JAVA_CONF > /dev/null 2>&1 &
-                #/usr/lib/jvm/java-11/bin/java $JAVA_OPT -classpath $PATH_TO_JAR DashServerMain $JAVA_CONF > /dev/null 2>&1 &
-                echo "$SERVICE_NAME started ..."
+                echo "[$SERVICE_NAME] started ..."
         fi
 }
 
@@ -34,9 +29,9 @@ function exec_stop() {
 	PID=`ps -ef | grep java | grep DashServerMain | awk '{print $2}'`
 	if [ -z "$PID" ]
 	then
-		echo "DASH is not running"
+		echo "[$SERVICE_NAME] is not running"
 	else
-		echo "stopping DASH"
+		echo "stopping [$SERVICE_NAME]"
 		kill "$PID"
 		sleep 1
 		PID=`ps -ef | grep java | grep DashServerMain | awk '{print $2}'`
@@ -45,7 +40,7 @@ function exec_stop() {
 			echo "kill -9 ${PID}"
 			kill -9 "$PID"
 		fi
-		echo "DASH stopped"
+		echo "[$SERVICE_NAME] stopped"
 	fi
 }
 
@@ -53,8 +48,9 @@ function exec_status() {
   PID=`ps -ef | grep java | grep DashServerMain | awk '{print $2}'`
 	if [ -z "$PID" ]
 	then
-		echo "DASH is not running"
+		echo "[$SERVICE_NAME] is not running"
 	else
+	  echo "[$SERVICE_NAME] is running"
 	  ps -aux | grep DashServerMain | grep "$PID"
 	fi
 }
